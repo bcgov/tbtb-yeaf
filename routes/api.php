250d76597f27d10c2b67bec856f10baec3a4caf0
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-10-24 12:17:03 -0700

    adds reports export buttons

--- routes/api.php
+++ routes/api.php
@@ -13,34 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/login', [App\Http\Controllers\UserController::class, 'apiLogin'])->name('api-login');
 
-Route::get('/students',
-    function (Request $request) {
-        return Response::json(\App\Models\Student::get());
-    }
-)->name('api.students');
-
-Route::get('/students-with-grants',
-    function (Request $request) {
-        return Response::json(\App\Models\Student::with('grants.grantIneligibles', 'comments', 'grants.appeals', 'grants.py')->get());
-    }
-)->name('api.students-with-grants');
-
-Route::get('/staff',
-    function (Request $request) {
-        return Response::json(\App\Models\User::get());
-    }
-)->name('api.staff');
-
-Route::get('/grants',
-    function (Request $request) {
-        return Response::json(\App\Models\Grant::get());
-    }
-)->name('api.grants');
-
-Route::get('/comments',
-    function (Request $request) {
-        return Response::json(\App\Models\Grant::get());
-    }
-)->name('api.comments');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
